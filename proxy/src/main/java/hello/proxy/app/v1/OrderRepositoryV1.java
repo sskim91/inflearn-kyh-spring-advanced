236862ba2a04725120ce0ba31a70e9cb74540481
sskim91 <tjdtjq91@gmail.com>
2022-04-02 23:42:54 +0900

    프록시 패턴과 데코레이터 패턴

---
+++ proxy/src/main/java/hello/proxy/app/v1/OrderRepositoryV1.java
@@ -0,0 +1,8 @@
+package hello.proxy.app.v1;
+
+/**
+ * @author sskim
+ */
+public interface OrderRepositoryV1 {
+    void save(String itemId);
+}
---
+++ proxy/src/main/java/hello/proxy/app/v1/OrderServiceV1.java
@@ -0,0 +1,8 @@
+package hello.proxy.app.v1;
+
+/**
+ * @author sskim
+ */
+public interface OrderServiceV1 {
+    void orderItem(String itemId);
+}
